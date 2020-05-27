1e4a3f8f08c4023bdbadbae328a41098931a9bd3
Maxim <Maxim27r@outlook.com>
2020-05-27 18:08:03 +0300

    Создание моделей

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField("Имя",max_length=30)
+    last_name = models.CharField("Фамилия",max_length=30)
+    phone_number = models.CharField("Номер телефона", max_length=15,blank=True)
+    email = models.EmailField("Почта",blank=True)
+    def __str__(self):
+        return self.first_name + " " + self.last_name
