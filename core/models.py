2d0a8213fe57f28edbfe887908db0cd6d62309e9
Maxim <Maxim27r@outlook.com>
2020-05-29 01:36:36 +0300

    Тестовый коммит через vs code

--- core/models.py
+++ core/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
 class Contact(models.Model):
-    first_name = models.CharField("Имя",max_length=30)
-    last_name = models.CharField("Фамилия",max_length=30)
-    phone_number = models.CharField("Номер телефона", max_length=15,blank=True)
-    email = models.EmailField("Почта",blank=True)
+    first_name = models.CharField("Имя", max_length=30)
+    last_name = models.CharField("Фамилия", max_length=30)
+    phone_number = models.CharField("Номер телефона", max_length=15, blank=True)
+    email = models.EmailField("Почта", blank=True)
+    description = models.TextField("Описание", blank=True)
+
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+    def sayHi(self):
+        return print("hi everybody my name is Maxim, and this is my first day using vs code with wsl2")
+
+class Lead(models.Model):
+    name = models.CharField("ФИО клиента", max_length=50, blank=True)
+    title = models.CharField("Название лида", max_length=50, default="Лид №"+str(id))
+    phone_number = models.CharField("Номер телефона", max_length=15, blank=True)
+    email = models.EmailField("Почта", blank=True)
+    contact = models.ForeignKey("Contact", on_delete=models.SET_NULL, null=True)
+    description = models.TextField("Описание", blank=True)
+
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead, Contact
 
 def index(request):
+    #print(Lead.objects.all()[0].contact.last_name)
     return render(request, "index.html")
